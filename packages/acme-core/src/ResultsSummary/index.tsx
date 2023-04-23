6f070f68a294d46962b69dbc132d948ac891470a
Michael-Bryant Choa <mbchoa@gmail.com>
2023-04-23 16:16:57 -0700

    tweak colors

--- packages/acme-core/src/ResultsSummary/index.tsx
+++ packages/acme-core/src/ResultsSummary/index.tsx
@@ -23,24 +23,24 @@ export const ResultsSummary = ({ scores }: IResultsSummaryProps) => {
         className={clsx(
           "flex flex-col flex-1 items-center",
           "rounded-b-[30px] sm:rounded-[30px]",
-          "bg-gradient-to-b from-results-summary-violet-blue to-results-summary-light-royal-blue to-15%",
+          "bg-gradient-to-b from-results-summary-light-slate-blue to-results-summary-light-royal-blue",
           "text-center",
           "p-8 space-y-4 sm:space-y-6",
           "text-white"
         )}
       >
-        <p className="text-gray-400">Your Result</p>
+        <p className="text-results-summary-pale-blue">Your Result</p>
         <div
           className={clsx(
             "flex flex-col justify-center items-center",
             "w-[130px] h-[130px] sm:w-[150px] sm:h-[150px]",
             "rounded-full",
-            "bg-gradient-to-b from-results-summary-cobalt-blue to-transparent shadow-inner",
+            "bg-gradient-to-b from-results-summary-violet-blue to-results-summary-persian-blue shadow-inner",
             "space-y-3"
           )}
         >
           <p className="text-5xl font-bold mt-5">76</p>
-          <p className="text-results-summary-light-lavender text-sm opacity-50">
+          <p className="text-results-summary-pale-blue text-sm opacity-50">
             of 100
           </p>
         </div>
