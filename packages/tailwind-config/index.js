2028773df76deb27fee7319e60a0e306ce797ab0
Michael-Bryant Choa <mbchoa@gmail.com>
2023-04-23 10:06:11 -0700

    add font

--- packages/tailwind-config/index.js
+++ packages/tailwind-config/index.js
@@ -7,6 +7,10 @@ module.exports = {
     fontFamily: {
       ...defaultConfig.theme.fontFamily,
       outfit: ["Outfit", ...defaultConfig.theme.fontFamily.sans],
+      "hanken-grotesk": [
+        "Hanken Grotesk",
+        ...defaultConfig.theme.fontFamily.sans,
+      ],
     },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
